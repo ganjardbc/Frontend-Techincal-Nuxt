28d8582f12d7c455610779e5a599083949680004
Ganjar Hadiatna <ganjardbc@gmail.com>
2023-12-14 11:13:02 +0700

    update git log

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/Frontend-Techincal-Nuxt/',
+    buildAssetsDir: 'assets',
+  },
   devServer: {
     port: 4000,
   },
