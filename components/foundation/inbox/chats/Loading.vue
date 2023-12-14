cceeb75b977ef429b5d611bf7570d721587558e9
Ganjar Hadiatna <ganjardbc@gmail.com>
2023-12-14 10:04:53 +0700

    change loading

--- components/foundation/inbox/chats/Loading.vue
+++ components/foundation/inbox/chats/Loading.vue
@@ -2,7 +2,7 @@
     <div class="foundation__chat-floating">
         <div class="foundation__chat-loading flex items-center" style="display: flex;">
             <div class="mr-4 ml-2">
-                <i class="fa fa-lg fa-spin fa-spinner text-custom__color-blue"></i>
+                <i class="fa fa-lg fa-spin fa-circle-notch text-custom__color-blue"></i>
             </div>
             <div class="font-sans font-semibold text-custom__color-gray text-custom__size-14px">
                 Please wait while we connect you with one of our team ...
--- components/foundation/inbox/list/Loading.vue
+++ components/foundation/inbox/list/Loading.vue
@@ -3,7 +3,7 @@
         <div class="foundation__loading flex-col items-center justify-center">
             <div class="flex-col items-center" style="display: flex;">
                 <div class="mb-4">
-                    <i class="fa fa-4x fa-spin fa-spinner text-custom__color-gray"></i>
+                    <i class="fa fa-4x fa-spin fa-circle-notch text-custom__color-gray"></i>
                 </div>
                 <div class="font-sans font-semibold text-custom__color-gray">
                     Loading Chats ..
